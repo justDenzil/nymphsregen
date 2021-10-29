2694f4ebe81985c700161c7388740fd63481136f
justDenzil <vikash.denzil@gmail.com>
2021-10-29 23:33:34 +0530

    box2

---
+++ components/ShowBox.jsx
@@ -0,0 +1,27 @@
+import React, {Suspense} from 'react';
+import { useGLTF, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber"
+import Model from "./NymphsRegenBoxforBrowser"
+
+
+
+//function Model() {
+  //const { scene } = useGLTF("nymphsRegenBoxforBrowser.gltf")
+  //return <primitive object={scene} />;
+//}
+  
+
+export function ShowBox(props) {
+  return (
+    <div style = {{height:"60vh"}}>
+      <Canvas camera={{position: [10, 18, 23], fov: 0.5 }}>
+      <ambientLight intensity={0.5} />
+      <pointLight color="white" intensity={1.5} position={[10, 10, 10]} />
+        <OrbitControls autoRotate/>
+        <Suspense fallback={null}>
+          <Model scale={[0.07,0.07,0.07]}/>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
